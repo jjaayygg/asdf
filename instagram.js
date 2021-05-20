86eee370c0cabea39853bb4d773589beaeba84a8
jjaayygg <83363760+jjaayygg@users.noreply.github.com>
2021-05-20 07:57:37 +0530

    l

--- instagram.js
+++ instagram.js
@@ -16,10 +16,10 @@ const { username, password } = {username:'photo_mania_5053', password:'pjay32547
       'https://source.unsplash.com/user/erondu/1080x1080'
    
     await client.login()
-   while(true){
+ //  while(true){
     // Upload Photo to feed or story, just configure 'post' to 'feed' or 'story'
     const { media } = await client.uploadPhoto({ photo: photo, caption: '#photooftheday #instagood #nofilter #picoftheday #love #nature #swag #lifeisgood #instadaily #selfie #instamood #bestoftheday #followme #likeforlike #like4like #follow4follow #followforfollow #f4f #instafood #foodporn #food #foodie #goodeats #vegetarian #vegan #friendship #hungry #homemade #travel #vacation', post: 'feed' })
     console.log(`https://www.instagram.com/p/${media.code}/`)
-   }
+//   }
   })()
 console.log('last line')
